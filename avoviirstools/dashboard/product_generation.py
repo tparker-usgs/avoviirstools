990e17a594d7ea9c75a6e239c8c4eb844fa449cf
Tom Parker <tparker@usgs.gov>
2019-05-29 17:29:57 -0800

    tweak pane org

--- avoviirstools/dashboard/product_generation.py
+++ avoviirstools/dashboard/product_generation.py
@@ -2,42 +2,44 @@ from dash.dependencies import Input, Output
 from .update_subscriber import UpdateSubscriber
 from .app import zmq_context, app
 
+update_subscriber = UpdateSubscriber(zmq_context)
 
 class ProductGeneration:
     def __init__(self):
-        self.update_subscriber = UpdateSubscriber(zmq_context)
-        self.update_subscriber.start()
+        update_subscriber.start()
 
     def fluch(self):
-        self.update_subscriber.flush()
+        update_subscriber.flush()
 
-    @app.callback(
-        Output("products-waiting", "figure"),
-        [Input("products-waiting-update", "n_intervals")],
-    )
-    def gen_products_waiting(self, interval):
-        waiting_tasks = self.update_subscriber.updates
-        figure = {
-            "data": [
-                {
-                    "x": waiting_tasks.index,
-                    "y": waiting_tasks,
-                    "type": "scatter",
-                    "name": "Products Waiting",
-                    "fill": "tozeroy",
-                }
-            ],
-            "layout": {
-                "xaxis": {"type": "date", "rangemode": "nonnegative"},
-                "title": "Products waiting to be generated",
-            },
-        }
 
-        return figure
+@app.callback(
+    Output("products-waiting", "figure"),
+    [Input("products-waiting-update", "n_intervals")],
+)
+def gen_products_waiting(interval):
+    waiting_tasks = update_subscriber.updates
+    figure = {
+        "data": [
+            {
+                "x": waiting_tasks.index,
+                "y": waiting_tasks,
+                "type": "scatter",
+                "name": "Products Waiting",
+                "fill": "tozeroy",
+            }
+        ],
+        "layout": {
+            "xaxis": {"type": "date", "rangemode": "nonnegative"},
+            "title": "Products waiting to be generated",
+        },
+    }
 
-    @app.callback(
-        Output("products-waiting-update", "disabled"),
-        [Input("products-waiting-auto", "values")],
-    )
-    def update_refresh(auto_values):
-        return "Auto" not in auto_values
+    return figure
+
+
+@app.callback(
+    Output("products-waiting-update", "disabled"),
+    [Input("products-waiting-auto", "values")],
+)
+def update_refresh(auto_values):
+    return "Auto" not in auto_values
