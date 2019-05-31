c2dfc7fe54f92a32d885a44d54174ef4fe4cb7a5
Tom Parker <tparker@usgs.gov>
2019-05-31 14:20:38 -0800

    add images chart

--- avoviirstools/dashboard/volcview_images.py
+++ avoviirstools/dashboard/volcview_images.py
@@ -19,10 +19,4 @@ class VolcviewImages:
 def gen_datafile_gap(n_clicks):
     data = sector_subscriber.sector_images.groupby("sector").size()
 
-    return {
-        "data": {"x": data.index, "y": data, "type": "bar", "name": "num images"},
-        "layout": {
-            "xaxis": {"type": "date", "rangemode": "nonnegative"},
-            "yaxis": {"title": "Interfile Gap (min)", "range": [0, 500]},
-        },
-    }
+    return {"data": [{"x": data.index, "y": data, "type": "bar", "name": "num images"}]}
