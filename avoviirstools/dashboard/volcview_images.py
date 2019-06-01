f6bf51a9c97c6817998fb3d68d8025725e2699da
Tom Parker <tparker@usgs.gov>
2019-05-31 17:38:05 -0800

    convert to days

--- avoviirstools/dashboard/volcview_images.py
+++ avoviirstools/dashboard/volcview_images.py
@@ -1,6 +1,8 @@
 from dash.dependencies import Input, Output
 from .sector_subscriber import SectorSubscriber
 from .app import zmq_context, app
+import pandas as pd
+
 
 sector_subscriber = SectorSubscriber(zmq_context)
 
@@ -17,21 +19,35 @@ class VolcviewImages:
     Output("volcview-sectors", "figure"), [Input("volcview-sectors-update", "n_clicks")]
 )
 def gen_volcview_sectors(n_clicks):
+    pdnow = pd.to_datetime("now")
+    yesterday = pdnow - pd.Timedelta("1 days")
+    today_data = sector_subscriber.sector_images[yesterday:pdnow]
+    today_data = today_data.groupby("sector").size()
+
     data = sector_subscriber.sector_images.groupby("sector").size()
+    days = data.index.max() - data.index.min()
+    days = days.days
+    if days > 0:
+        data = data / days
 
     return {
         "data": [
-            {"x": data.index, "y": data, "type": "bar", "name": "num images"},
-            {"x": data.index, "y": data, "type": "scatter", "name": "avg images"},
+            {"x": data.index, "y": today_data, "type": "bar", "name": "num images"},
+            {
+                "x": data.index,
+                "y": data,
+                "type": "scatter",
+                "name": "avg images",
+                "mode": "markers",
+            },
         ],
         "layout": {"title": "Images by Sector"},
     }
 
-
-@app.callback(
     Output("volcview-products", "figure"),
     [Input("volcview-products-update", "n_clicks")],
-)
+
+
 def gen_volcview_products(n_clicks):
     data = sector_subscriber.sector_images.groupby("band").size()
 
