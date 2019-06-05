b6730c468223922770a9e77bf45c59284068b607
Tom Parker <tparker@usgs.gov>
2019-06-05 14:59:33 -0800

    add bootstrap js

--- avoviirstools/dashboard/app.py
+++ avoviirstools/dashboard/app.py
@@ -7,10 +7,34 @@ import time
 import flask
 
 PICKLING_INTERVAL = 5 * 60
+
+external_scripts = [
+    {
+        "src": "https://code.jquery.com/jquery-3.2.1.slim.min.js",
+        "integrity": "sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN",
+        "crossorigin": "anonymous",
+    },
+    {
+        "src": "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js",
+        "integrity": "sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q",
+        "crossorigin": "anonymous",
+    },
+    {
+        "src": "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js",
+        "integrity": "sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl",
+        "crossorigin": "anonymous",
+    },
+]
+
 external_css = [
     "https://fonts.googleapis.com/css?family=Merriweather:300&display=swap",
-    "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    {
+        "href": "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+        "rel": "stylesheet",
+        "integrity": "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T",
+        "crossorigin": "anonymous",
+    },
 ]
 
 
@@ -41,7 +65,12 @@ def gen_layout():
 
 flusher = Flusher()
 server = flask.Flask(__name__)
-app = dash.Dash(__name__, server=server, external_stylesheets=external_css)
+app = dash.Dash(
+    __name__,
+    server=server,
+    external_scripts=external_scripts,
+    external_stylesheets=external_css,
+)
 app.layout = gen_layout()
 init_callbacks(flusher)
 flusher.start()
