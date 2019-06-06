01f338ec73b70b3699fab1be9acfbb54b02b8e60
Tom Parker <tparker@usgs.gov>
2019-06-05 17:20:45 -0800

    use local stylesheet

--- avoviirstools/dashboard/app.py
+++ avoviirstools/dashboard/app.py
@@ -35,6 +35,7 @@ external_css = [
         "integrity": "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T",  # NOQA: E501
         "crossorigin": "anonymous",
     },
+    "/assets/style.css",
 ]
 
 
