90007b0ee4eb705efda272b379db13a29f80d650
Tom Parker <tparker@usgs.gov>
2019-07-11 10:01:58 -0800

    add version tag to dashboard page

--- avoviirstools/dashboard/layout/__init__.py
+++ avoviirstools/dashboard/layout/__init__.py
@@ -1,4 +1,5 @@
 import dash_html_components as html
+import avoviirstools.version
 from .volcview_images import volcview_images_layout
 from .product_generation import product_generation_layout
 from .data_arrival import data_arrival_layout
@@ -14,6 +15,7 @@ def gen_layout():
             volcview_images_layout(),
             product_generation_layout(),
             data_arrival_layout(),
+            html.Div(html.P("version: {}".format(avoviirstools.version.__version__))),
         ],
         className="container-fluid",
     )
